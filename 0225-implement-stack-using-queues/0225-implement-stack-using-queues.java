a281d802600af607aad45f9a0cd00a30ca3a7155
Deepanshi Goyal <deepanshigoyal12345@gmail.com>
2026-03-03 09:46:09 +0530

    Time: 0 ms (100%), Space: 43 MB (21.67%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,25 @@
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        
+        // Rotate the queue
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll()); //poll=first ele like peek and offer is for pushing ele to end of queue
+        }
+    }
+    public int pop() {
+        return q.poll();
+    }
+    public int top() {
+        return q.peek();
+    }
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
